a2537f4b1f27507e4b02808ee696f2ffb89f3149
masa <mi81ma@gmail.com>
2022-07-18 16:24:10 +0800

    Add WPF Dispatcher test code

---
+++ WpfDispatcherTestApp/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDispatcherTestApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            listBox1.Items.Clear();
+            Parallel.For(0, 10000, (i) =>
+            {
+                listBox1.Dispatcher.BeginInvoke(
+                    new Action(() =>
+                    {
+                        listBox1.Items.Add(i);
+                    }));
+            });
+
+            
+
+        }
+    }
+}
